065492ec6646e4807ecc708fdb471cccc468b9c6
Jose M Rico <jrico@princeton.edu>
2019-01-12 16:07:32 -0500

    Socket stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 #include <stdio.h>
+#include <sys/socket.h>
 
 int main(int argc, char* argv[])
 {
     printf("Hello, sailor\n");
+
+    int sockFD = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockFD < 0) {
+        fprintf(stderr, "Failed to open socket\n");
+        return 1;
+    }
 }
